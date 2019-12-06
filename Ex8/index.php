9c329add8b6b98f0ebfedb3d99dfd7b98467d03f
EmilieLamps <emilie.lamps@gmail.com>
2019-12-06 11:57:59 +0100

    Correction

--- Ex8/index.php
+++ Ex8/index.php
@@ -1,11 +1,8 @@
 <?php
 $page = "Exercice 8"; // Définir la variable pour changer le titre !
 $addition = 3 + 4;
-$multiplication = 5 * 20;
-$division = 45 / 5;
-$firstResult = $addition;
-$secondResult = $multiplication;
-$thirdResult = $division;
+$multiply = 5 * 20;
+$split = 45 / 5;
 include '../header.php';
  ?>
  <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 8</h2>
@@ -15,8 +12,8 @@ Dans la deuxième mettre le résultat de l'opération 5 * 20.
 Dans la troisième mettre le résultat de l'opération 45 / 5.
 Afficher le contenu des variables.</h3>
 <p>Le résultat de l'addition est <?= $addition ?></p>
-<p>Le résultat de la multiplication est <?= $multiplication ?></p>
-<p>Le résultat de la division est <?= $division ?></p>
+<p>Le résultat de la multiplication est <?= $multiply ?></p>
+<p>Le résultat de la division est <?= $split ?></p>
 <?php
 include '../footer.php';
 ?>
